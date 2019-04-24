1562816abdc7c1471a98d626eb1c3dfcb46d0ab9
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-24 09:33:09 -0700

    feat: Added starter code for views.py

--- views.py
+++ views.py
@@ -1,69 +1,41 @@
-#!/usr/bin/env python
-
-from models import Base, User
+from models import Base, User, Bagel
 from flask import Flask, jsonify, request, url_for, abort, g
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-
 from flask.ext.httpauth import HTTPBasicAuth
-auth = HTTPBasicAuth()
+
+auth = HTTPBasicAuth() 
 
 
-engine = create_engine('sqlite:///users.db')
+engine = create_engine('sqlite:///bagelShop.db')
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
-@auth.verify_password
-def verify_password(username, password):
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-    user = session.query(User).filter_by(username = username).first()
-    if not user or not user.verify_password(password):
-        return False
-    g.user = user
-    return True
+#ADD @auth.verify_password here
+
+#ADD a /users route here
 
 
-@app.route('/users', methods = ['POST'])
-def new_user():
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-    username = request.json.get('username')
-    password = request.json.get('password')
-    if username is None or password is None:
-        print "missing arguments"
-        abort(400) 
-        
-    if session.query(User).filter_by(username = username).first() is not None:
-        print "existing user"
-        user = session.query(User).filter_by(username=username).first()
-        return jsonify({'message':'user already exists'}), 200#, {'Location': url_for('get_user', id = user.id, _external = True)}
-        
-    user = User(username = username)
-    user.hash_password(password)
-    session.add(user)
-    session.commit()
-    return jsonify({ 'username': user.username }), 201#, {'Location': url_for('get_user', id = user.id, _external = True)}
 
-@app.route('/api/users/<int:id>')
-def get_user(id):
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-    user = session.query(User).filter_by(id=id).one()
-    if not user:
-        abort(400)
-    return jsonify({'username': user.username})
-
-@app.route('/api/resource')
-@auth.login_required
-def get_resource():
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-    return jsonify({ 'data': 'Hello, %s!' % g.user.username })
+@app.route('/bagels', methods = ['GET','POST'])
+#protect this route with a required login
+def showAllBagels():
+    if request.method == 'GET':
+        bagels = session.query(Bagel).all()
+        return jsonify(bagels = [bagel.serialize for bagel in bagels])
+    elif request.method == 'POST':
+        name = request.json.get('name')
+        description = request.json.get('description')
+        picture = request.json.get('picture')
+        price = request.json.get('price')
+        newBagel = Bagel(name = name, description = description, picture = picture, price = price)
+        session.add(newBagel)
+        session.commit()
+        return jsonify(newBagel.serialize)
 
 
 
