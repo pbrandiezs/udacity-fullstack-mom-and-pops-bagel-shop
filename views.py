a0009adb2ac6ade11638ffe88ea50bb37e04963f
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-24 09:36:07 -0700

    feat: Added @auth.verify_password route

--- views.py
+++ views.py
@@ -18,6 +18,15 @@ session = DBSession()
 app = Flask(__name__)
 
 #ADD @auth.verify_password here
+@auth.verify_password
+def verify_password(username, password):
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    user = session.query(User).filter_by(username = username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
 
 #ADD a /users route here
 
